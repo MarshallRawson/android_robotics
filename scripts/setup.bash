615da03933aa62513831711b9530fa1db9c2aa01
MarshallRawson <marshallrawson@ufl.edu>
2021-08-21 20:20:18 -0500

    removed start of cross compiling infrastructure in favor of just expanding memory on deivce

--- scripts/setup.bash
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-export ANDROID_ROBOTICS_REPO_ROOT="$(realpath $(dirname $BASH_SOURCE)/../)"
-export ANDROID_NDK=$ANDROID_ROBOTICS_REPO_ROOT/NDK/android-ndk
--- scripts/unsetup.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-unset ANDROD_ROBOTICS_REPO_DIR
-unset ANDROID9_CLANGpp
-unset ANDROID9_CLANG
